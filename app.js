d227b8044e0590f2ee482c0b7c8a9bff56a83106
Justin Carrus <carrus.justin@gmail.com>
2016-02-01 05:27:33 +0000

    Revisions and add approach page

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ app.get('/rockers', function(req, res){
 app.get('/tracksim', function(req, res){
     res.render('tracksim', {});
 });
-app.get('/cad', function(req, res){
-    res.render('cad', {});
+app.get('/approach', function(req, res){
+    res.render('approach', {});
 });
 app.get('/charcoal', function(req, res){
     res.render('charcoal', {});
