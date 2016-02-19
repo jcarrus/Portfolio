8c0e702d6e5c456495ead8029705a16bce4f36b7
Justin Carrus <carrus.justin@gmail.com>
2016-02-19 07:26:05 +0000

    Updates to logger

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var stylus = require('stylus');
 var nib = require('nib');
 var jade = require('jade');
+var where = require('node-where');
 
 var app = express();
 
@@ -14,6 +15,33 @@ function compile(str, path) {
     .set('compress', true)
     .use(nib());
 }
+
+app.use(function(req, res, next){
+    if (req.path.indexOf('/public/') != -1) return next();
+    where.is(req.ip, function(err, result) {
+	if (result){
+	    var d = new Date();
+	    var str = d.toUTCString() + '\t' + 
+		(req.ip || 'ip') + '\tfrom\t ' + 
+		(result.attributes.city || 'city') + '\t' + 
+		(result.attributes.region || 'region')  + '\t' + 
+		(result.attributes.country || 'country')  + '\trequests\t' + 
+		req.path;
+	    console.log(str);
+	    str += '\n';
+	    fs.appendFile(__dirname + '/portfolio.log', str, function(err){
+		if (err) throw err;
+	    });
+	} else {
+	    try{
+		console.log(err);
+	    } catch (e){
+		
+	    }
+	}
+    });
+    next();
+});
 
 app.use(stylus.middleware({
     src: __dirname + '/public'
@@ -27,15 +55,16 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', function(req, res){
     res.render('index', {});
 });
-app.get('/about', function(req, res){
-    res.render('about', {});
-});
 app.get('/resume', function(req, res){
     var resume=__dirname + "/public/JustinCarrus.pdf";
     fs.readFile(resume, function (err,data){
 	res.contentType("application/pdf");
 	res.send(data);
     });
+});
+
+app.get('/about', function(req, res){
+    res.render('about', {});
 });
 app.get('/daq', function(req, res){
     res.render('daq', {});
@@ -55,14 +84,6 @@ app.get('/process', function(req, res){
 app.get('/charcoal', function(req, res){
     res.render('charcoal', {});
 });
-
-// Budget Application
-app.get('/budget', function(req, res){
-    res.sendFile(__dirname + '/views/Budget.html');
-});
-
-// Freshman stalking app
-//app.use('/rush', require('./freshman-stalking/rushWeek/app').app);
 
 if (isLocal){
     app.listen(3000, function() {
